77686c6b2151ab5a26cb086bfd1237c002270723
Anh Nguyen <anhnn2011@gmail.com>
2026-08-20 10:51:14 +0700

    feat: load active provider from KOCloud config

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+local Config = require("core/config")
 local GoogleDriveProvider = require("providers/google_drive/provider")
 local InfoMessage = require("ui/widget/infomessage")
 local UIManager = require("ui/uimanager")
@@ -6,15 +7,30 @@ local _ = require("gettext")
 
 --- Main KOCloud plugin container.
 ---@class KOCloudPlugin: WidgetContainer
+---@field config KOCloudConfig
 ---@field provider KOCloudGoogleDriveProvider
 local KOCloud = WidgetContainer:extend{
     name = "kocloud",
     is_doc_only = false,
 }
 
---- Initialize KOCloud and register it in the KOReader main menu.
+--- Initialize KOCloud, load configuration, create the active provider,
+--- and register the plugin in the KOReader main menu.
 function KOCloud:init()
-    self.provider = GoogleDriveProvider:new()
+    self.config = Config:new()
+
+    local provider_type = self.config:getActiveProvider()
+    local provider_config = self.config:getProviderConfig(provider_type)
+
+    if provider_type == "google_drive" then
+        self.provider = GoogleDriveProvider:new(provider_config)
+    else
+        error(string.format(
+            "Unsupported KOCloud provider: %s",
+            provider_type
+        ))
+    end
+
     self.ui.menu:registerToMainMenu(self)
 end
 
@@ -27,9 +43,14 @@ function KOCloud:addToMainMenu(menu_items)
         callback = function()
             UIManager:show(InfoMessage:new{
                 text = string.format(
-                    _("Provider: %s\nType: %s"),
+                    _(
+                        "Provider: %s\n"
+                            .. "Type: %s\n"
+                            .. "Settings: %s"
+                    ),
                     self.provider:getName(),
-                    self.provider:getType()
+                    self.provider:getType(),
+                    self.config:getSettingsFile()
                 ),
             })
         end,
