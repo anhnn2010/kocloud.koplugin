eb261af3ed9b57e4719180832223811fa041da94
Anh Nguyen <anhnn2011@gmail.com>
2026-08-20 10:45:13 +0700

    test: wire Google Drive provider into KOCloud

--- main.lua
+++ main.lua
@@ -1,24 +1,36 @@
+local GoogleDriveProvider = require("providers/google_drive/provider")
 local InfoMessage = require("ui/widget/infomessage")
 local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local _ = require("gettext")
 
+--- Main KOCloud plugin container.
+---@class KOCloudPlugin: WidgetContainer
+---@field provider KOCloudGoogleDriveProvider
 local KOCloud = WidgetContainer:extend{
     name = "kocloud",
     is_doc_only = false,
 }
 
+--- Initialize KOCloud and register it in the KOReader main menu.
 function KOCloud:init()
+    self.provider = GoogleDriveProvider:new()
     self.ui.menu:registerToMainMenu(self)
 end
 
+--- Add KOCloud to the KOReader main menu.
+---@param menu_items table
 function KOCloud:addToMainMenu(menu_items)
     menu_items.kocloud = {
         text = _("KOCloud"),
         sorting_hint = "more_tools",
         callback = function()
             UIManager:show(InfoMessage:new{
-                text = _("KOCloud plugin loaded successfully."),
+                text = string.format(
+                    _("Provider: %s\nType: %s"),
+                    self.provider:getName(),
+                    self.provider:getType()
+                ),
             })
         end,
     }
